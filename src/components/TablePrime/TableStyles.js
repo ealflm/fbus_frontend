f56fb4ef7e6e23a6a856edd764755930d5764504
dtsangdtd <49357143+dtsangdtd@users.noreply.github.com>
2022-11-08 15:34:03 +0700

    Delete src directory

--- src/components/TablePrime/TableStyles.js
+++
@@ -1,9 +0,0 @@
-import { makeStyles, createStyles } from '@mui/styles';
-
-export const useTableStyles = makeStyles(() =>
-  createStyles({
-    ButtonExport: {
-      height: '20px',
-    },
-  })
-);
