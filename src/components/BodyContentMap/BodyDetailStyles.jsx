fa97637ea6eb1113792e3494c230317a3aef3a2d
sangdt <sangdt@unit.com.vn>
2022-11-30 10:37:25 +0700

    fix css map

--- src/components/BodyContentMap/BodyDetailStyles.jsx
+++ src/components/BodyContentMap/BodyDetailStyles.jsx
@@ -1,36 +1,36 @@
-import { createStyles, makeStyles } from '@mui/styles';
+import { createStyles, makeStyles } from "@mui/styles";
 
 export const useBodyDetailStyles = makeStyles(() =>
   createStyles({
     boxBodyDetail: {
-      position: 'absolute',
-      width: '300px',
-      backgroundColor: 'rgba(255, 255, 255, 0.88)',
-      right: '20px',
-      top: '17%',
+      position: "absolute",
+      width: "300px",
+      backgroundColor: "rgba(255, 255, 255, 0.88)",
+      right: "20px",
+      top: "130px",
       zIndex: 999,
-      borderRadius: '9px',
+      borderRadius: "9px",
     },
     boxBodyTitle: {
-      marginLeft: '10px',
+      marginLeft: "10px",
     },
     contentBoxBody: {
-      marginTop: '10px',
-      height: 'auto',
+      marginTop: "10px",
+      height: "auto",
     },
     bottomContentBody: {
       // position: 'absolute',
       // right: '30px',
       // bottom: '10px',
-      padding: '10px',
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
+      padding: "10px",
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
     },
     cardBodyList: {
-      display: 'flex',
-      flexDirection: 'column',
-      gap: '10px 0px',
+      display: "flex",
+      flexDirection: "column",
+      gap: "10px 0px",
     },
   })
 );
--- src/components/BodyContentMap/BodyListStyles.jsx
+++ src/components/BodyContentMap/BodyListStyles.jsx
@@ -1,39 +1,40 @@
-import { createStyles, makeStyles } from '@mui/styles';
+import { createStyles, makeStyles } from "@mui/styles";
 
 export const useBodyListStyles = makeStyles(() =>
   createStyles({
     boxBodyList: {
-      position: 'absolute',
-      height: '75vh',
-      width: '300px',
-      backgroundColor: 'rgba(255, 255, 255, 0.88)',
-      right: '20px',
-      bottom: '20px',
+      position: "absolute",
+      height: "75vh",
+      width: "300px",
+      backgroundColor: "rgba(255, 255, 255, 0.88)",
+      top: "130px",
+      right: "20px",
+      bottom: "20px",
       zIndex: 999,
-      borderRadius: '9px',
+      borderRadius: "9px",
     },
     boxBodyTitle: {
-      marginLeft: '10px',
+      marginLeft: "10px",
     },
     contentBoxBody: {
-      marginTop: '10px',
+      marginTop: "10px",
       // backgroundColor: 'red',
-      height: '92%',
-      maxHeight: '92%',
+      height: "92%",
+      maxHeight: "92%",
     },
     scrollBodyContent: {
-      marginLeft: '10px',
-      marginTop: '10px',
+      marginLeft: "10px",
+      marginTop: "10px",
     },
     bottomContentBody: {
-      position: 'absolute',
-      right: '30px',
-      bottom: '10px',
+      position: "absolute",
+      right: "30px",
+      bottom: "10px",
     },
     cardBodyList: {
-      display: 'flex',
-      flexDirection: 'column',
-      gap: '10px 0px',
+      display: "flex",
+      flexDirection: "column",
+      gap: "10px 0px",
     },
   })
 );
